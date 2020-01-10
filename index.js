6b35fb2a3f915e46e81d3450de17204442c2f848
Natalie - Laptop <natalie1vm@hotmail.com>
2020-01-10 15:41:55 -0800

    Back to top and site links working

---
+++ index.js
@@ -0,0 +1,17 @@
+function topFunction()
+{
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$('#linkedin').on('click', ()=>{
+  window.open("https://www.linkedin.com/in/natalie-van-maerssen-845924148", "_blank");
+});
+
+$('#instagram').on('click', ()=>{
+  window.open("https://www.instagram.com/artxnat_/", "_blank");
+});
+
+$('#github').on('click', ()=>{
+  window.open("https://github.com/NatColeKing/", "_blank");
+});
