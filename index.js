2ffd4b0257d36de5870cf4213e1385242d6acdc7
Natalie - Laptop <natalie1vm@hotmail.com>
2020-01-13 17:07:45 -0800

    Individual divs created

--- index.js
+++ index.js
@@ -15,3 +15,44 @@ $('#instagram').on('click', ()=>{
 $('#github').on('click', ()=>{
   window.open("https://github.com/NatColeKing/", "_blank");
 });
+
+$('#work_cat_name').on('click', ()=>{
+  hideAllMainViewBut('gallery');
+});
+
+$('#vr_cat_name').on('click', ()=>{
+  hideAllMainViewBut('vr_portfolio');
+});
+
+$('#mod_cat_name').on('click', ()=>{
+  hideAllMainViewBut('mod_portfolio');
+});
+
+$('#photo_cat_name').on('click', ()=>{
+  hideAllMainViewBut('photo_portfolio');
+});
+
+$('#vect_cat_name').on('click', ()=>{
+  hideAllMainViewBut('vect_portfolio');
+});
+
+$('#dig_cat_name').on('click', ()=>{
+  hideAllMainViewBut('dig_portfolio');
+});
+
+$('#personal_cat_name').on('click', ()=>{
+  hideAllMainViewBut('personal_info');
+});
+
+$('#contact_cat_name').on('click', ()=>{
+  hideAllMainViewBut('contact');
+});
+
+$('#cv_cat_name').on('click', ()=>{
+  hideAllMainViewBut('cv');
+});
+
+function hideAllMainViewBut(idName){
+  $('.main_view').addClass('hide');
+  $('#'+idName).removeClass('hide');
+}
