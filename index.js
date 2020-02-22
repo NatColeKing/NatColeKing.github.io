b62c45088187e444502a2dd6e7e139087c5b632a
Natalie - Laptop <natalie1vm@hotmail.com>
2020-02-22 00:31:16 -0800

    Started 'about me' page

--- index.js
+++ index.js
@@ -20,34 +20,6 @@ $('#github').on('click', ()=>{
 //   hideAllMainViewBut('#gallery_inner');
 // });
 //
-// $('.vr_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#vr_portfolio');
-// });
-//
-// $('.photo_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#photo_portfolio');
-// });
-//
-// $('.cs_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#cs_portfolio');
-// });
-//
-// $('.dig_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#dig_portfolio');
-// });
-//
-// $('#about_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#about');
-// });
-//
-// $('#contact_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#contact');
-// });
-//
-// $('#cv_cat_name').on('click', ()=>{
-//   hideAllMainViewBut('#cv');
-// });
-//
 // function hideAllMainViewBut(query){
 //   $('.main_view').addClass('hide');
 //   $(query).removeClass('hide');
